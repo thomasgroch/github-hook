eff9308323fea1c4faa7f9fa86ea01b6345c4ea3
Thomas Letsch Groch <thomas.groch@gmail.com>
2020-10-20 21:25:02 -0300

    paths updated

--- app.js
+++ app.js
@@ -42,55 +42,7 @@ app.post('/payload', function (req, res) {
 app.listen(5000, function () {
 	console.log('listening on port 5000')
 });
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var exec = require('child_process').exec;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.htm');
-	console.log('get /');
-});
-
-app.get('/payload', function (req, res) {
-    res.sendStatus(200);
-	console.log('get /payload');
-});
-
-app.post('/payload', function (req, res) {
-	//verify that the payload is a push from the correct repo
-	//verify repository.name == 'wackcoon-device' or repository.full_name = 'DanielEgan/wackcoon-device'
-	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
-
-	console.log('pulling code from GitHub...');
-
-	// reset any changes that have been made locally
-	exec('git -C ~/pi-planta reset --hard', execCallback);
-
-	// and ditch any files that have been added locally too
-	exec('git -C ~/pi-planta clean -df', execCallback);
-
-	// now pull down the latest
-	exec('git -C ~/pi-planta pull -f', execCallback);
-
-	// and npm install with --production
-	exec('npm -C ~/pi-planta install --production', execCallback);
-	
-	// and run the project
-	exec('npm run start', execCallback);
-});
-
-app.listen(5000, function () {
-	console.log('listening on port 5000')
-});
-
-function execCallback(err, stdout, stderr) {
-	if(stdout) console.log(stdout);
-	if(stderr) console.log(stderr);
-}U
 function execCallback(err, stdout, stderr) {
 	if(stdout) console.log(stdout);
 	if(stderr) console.log(stderr);
