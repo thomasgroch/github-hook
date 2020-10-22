82527f2ece05639d8cb77eb7030941bf3523bf8f
Thomas Letsch Groch <thomas.groch@gmail.com>
2020-10-22 14:16:17 -0300

    fix

--- restart.sh
+++ restart.sh
@@ -4,5 +4,5 @@ echo "$(date) Killing the processes"
 [ -e /home/pi/pids/lt_github_hook.pid ] && kill $(cat /home/pi/pids/lt_github_hook.pid)
 sleep 3s
 
-echo "$(date) Starting the app"
+echo "$(date) Starting github-hook tunnel"
 /usr/local/bin/lt --subdomain 'github-hook' --port 5000 > /home/pi/lt_stdout.txt 2> /home/pi/lt_stderr.txt & echo $! > /home/pi/pids/lt_github_hook.pid
