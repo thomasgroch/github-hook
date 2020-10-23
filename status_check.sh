da48a2694e45cc698edf1a9ef21422bd3dc0c401
Thomas Letsch Groch <thomas.groch@gmail.com>
2020-10-23 14:25:05 -0300

    fix

--- status_check.sh
+++ status_check.sh
@@ -10,11 +10,11 @@ log() {
 log "Status check ${APP} on port ${PORT}"
 touch "${HOME}/lt_${APP}.stdout"
 
-if [[ $(ps aux | grep "${APP}") ]]; then
-  log "Service is running"
-elif [[ $(cat "${HOME}/lt_${APP}.stdout"| grep "${APP}") ]]; then
+if [[ $(cat "${HOME}/lt_${APP}.stdout"| grep "${APP}") ]]; then
   log "Wrong domain. Restarting Service."
   cd "${HOME}/${APP}/" && sh restart.sh $1 $2
+elif [[ $(ps aux | grep "${APP}") ]]; then
+  log "Service is running"
 else
   log "Service is stopped. Starting"
   cd "${HOME}/${APP}/" && sh restart.sh $1 $2
