4b583a30ea6e5d684c83d2a526351fcd144f658b
Thomas Letsch Groch <thomas.groch@gmail.com>
2020-10-23 12:11:45 -0300

    better tunnel script

--- status_check.sh
+++ status_check.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 APP=$1
+PORT=$2
 log "$(date) $APP Status check"
 
 log() {
     echo "$(date) $1"
 }
-restart() {
-    cd $HOME/$APP/ && sh restart.sh
-}
 
 if [[ $(ps aux | grep '$APP') ]]; then
   log "Service is running"
 elif [[ $(cat "${HOME}/lt_${APP}.stdout"| grep '$APP') ]]; then
-  log "Wrong domain. Restarting Service."; restart
+  log "Wrong domain. Restarting Service."
+  cd $HOME/$APP/ && sh restart.sh $1 $2
 else
-  log "Service is stopped. Starting"; restart
+  log "Service is stopped. Starting"
+  cd $HOME/$APP/ && sh restart.sh $1 $2
 fi
