38e1535cfad392c5ee8c71a6ca2e1eceffb93a29
Thomas Letsch Groch <thomas.groch@gmail.com>
2020-10-23 12:34:02 -0300

    fix

--- restart.sh
+++ restart.sh
@@ -2,7 +2,7 @@
 
 APP=$1
 PORT=${2:-80}
-PID_PATH=$HOME/pids
+PID_PATH="${HOME}/pids"
 
 mkdir -p $PID_PATH; touch "${PID_PATH}/lt_${APP}.pid"
 log() {
@@ -10,11 +10,11 @@ log() {
 }
 
 log "Killing ${APP} process"
-[ -e $PID_PATH/$APP.pid ] && kill $(cat $PID_PATH/$APP.pid)
+[ -e "${PID_PATH}/${APP}.pid" ] && kill $(cat "${PID_PATH}/${APP}.pid")
 sleep 3s
 
-log "Starting ${APP} tunnel"
+log "Starting ${APP} tunnel on port ${PORT}"
 /usr/local/bin/lt --subdomain "${APP}" --port $PORT \
-	> $HOME/$APP.stdout \
-	2> $HOME/$APP.stderr \
-	& echo $! > $PID_PATH/$APP.pid
+	> "${HOME}/${APP}.stdout" \
+	2> "${HOME}/${APP}.stderr" \
+	& echo $! > "${PID_PATH}/${APP}.pid"
--- status_check.sh
+++ status_check.sh
@@ -11,10 +11,10 @@ log "Status check ${APP} on port ${PORT}"
 
 if [[ $(ps aux | grep '$APP') ]]; then
   log "Service is running"
-elif [[ $(cat "${HOME}/lt_${APP}.stdout"| grep '$APP') ]]; then
+elif [[ $(cat "${HOME}/lt_${APP}.stdout"| grep "${APP}") ]]; then
   log "Wrong domain. Restarting Service."
-  cd $HOME/$APP/ && sh restart.sh $1 $2
+  cd "${HOME}/${APP}/" && sh restart.sh $1 $2
 else
   log "Service is stopped. Starting"
-  cd $HOME/$APP/ && sh restart.sh $1 $2
+  cd "${HOME}/${APP}/" && sh restart.sh $1 $2
 fi
