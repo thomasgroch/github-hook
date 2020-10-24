852f05d46a904b02baa9786c3e6e14a90f3bc184
Thomas Letsch Groch <thomas.groch@gmail.com>
2020-10-24 13:31:40 -0300

    fixed

--- restart.sh
+++ restart.sh
@@ -2,21 +2,25 @@
 
 APP=$1
 PORT=${2:-80}
-PID_PATH="${HOME}/pids"
 
-mkdir -p $PID_PATH; touch "${PID_PATH}/lt_${APP}.pid"
-touch "${HOME}/${APP}.stderr"
-touch "${HOME}/${APP}.stdout"
+SCRIPT_HOME="${HOME}/lt"
+STDOUT_FILE="${SCRIPT_HOME}/${APP}.stdout"
+STDERR_FILE="${SCRIPT_HOME}/${APP}.stderr"
+PID_PATH="${SCRIPT_HOME}/pids"
+PID_FILE="${PID_PATH}/${APP}.pid"
+
+mkdir -p $PID_PATH; touch "${STDERR_FILE}" "${STDOUT_FILE}" "${PID_FILE}"
+
 log() {
     echo "[$(date)] $1"
 }
 
 log "Killing ${APP} process"
-[ -e "${PID_PATH}/lt_${APP}.pid" ] && kill $(cat "${PID_PATH}/lt_${APP}.pid")
+[ -e "${PID_FILE}" ] && kill $(cat "${PID_FILE}")
 sleep 3s
 
 log "Starting ${APP} tunnel on port ${PORT}"
 /usr/local/bin/lt --subdomain "${APP}" --port $PORT \
-	> "${HOME}/${APP}.stdout" \
-	2> "${HOME}/${APP}.stderr" \
-	& echo $! > "${PID_PATH}/lt_${APP}.pid"
+	> "${STDOUT_FILE}" \
+	2> "${STDERR_FILE}" \
+	& echo $! > "${PID_FILE}"
--- status_check.sh
+++ status_check.sh
@@ -3,18 +3,22 @@
 APP=$1
 PORT=$2
 
+SCRIPT_HOME="${HOME}/lt"
+LT_STDOUT_FILE="${SCRIPT_HOME}/${APP}.stdout"
+
 log() {
     echo "[$(date)] $1"
 }
 
 log "Status check ${APP} on port ${PORT}"
-touch "${HOME}/lt_${APP}.stdout"
 
-if [[ $(cat "${HOME}/lt_${APP}.stdout"| grep "${APP}") ]]; then
+touch "${LT_STDOUT_FILE}"
+if [[ $(cat "${LT_STDOUT_FILE}"| grep "${APP}") ]]; then
   log "Wrong domain. Restarting Service."
   cd "${HOME}/${APP}/" && sh restart.sh $1 $2
 elif [[ $(ps aux | awk "/[b]in\/lt .*${APP}/ {print $2}") ]]; then
   log "Service is running"
+  log "$(ps aux | awk "/[b]in\/lt .*${APP}/ {print $2}")"
 else
   log "Service is stopped. Starting"
   cd "${HOME}/${APP}/" && sh restart.sh $1 $2
