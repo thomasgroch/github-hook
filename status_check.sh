6eba177eb7db994708b360ebbc18107566d101c1
Thomas Letsch Groch <thomas.groch@gmail.com>
2020-10-24 13:53:28 -0300

    fixed

--- status_check.sh
+++ status_check.sh
@@ -5,6 +5,8 @@ PORT=$2
 
 SCRIPT_HOME="${HOME}/lt"
 LT_STDOUT_FILE="${SCRIPT_HOME}/${APP}.stdout"
+PID_PATH="${SCRIPT_HOME}/pids"
+PID_FILE="${PID_PATH}/${APP}.pid"
 
 log() {
     echo "[$(date)] $1"
@@ -17,7 +19,7 @@ touch "${LT_STDOUT_FILE}"
 if [[ $(cat "${LT_STDOUT_FILE}"| grep "${APP}") ]]; then
   log "Wrong domain. Restarting Service."
   cd "${HOME}/${APP}/" && sh restart.sh $1 $2
-elif [[ $(ps aux | awk "/[b]in\/lt .*${APP}/ {print $2}") ]]; then
+elif [[ -n "$(cat $PID_FILE)" ]]	; then
   log "Service is running"
 else
   log "Service is stopped. Starting"
